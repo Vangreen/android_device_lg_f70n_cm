638ff5d082ff54b2e08a26ed337ad8ac09229795
Paweł Wangryn <pwangryn@op.pl>
2016-04-30 16:31:26 +0200

    Update fixup.sh

--- prebuilt/fixup.sh
+++ prebuilt/fixup.sh
@@ -2,7 +2,7 @@
 
 model=`cat /proc/cmdline | sed 's/.*model.name=\([^ ]*\).*/\1/'`
 
-if [ "$model" = "LG-D618" ] || [ "$model" = "LG-D610" ]; then
+if [ "$model" = "LG-D315" ]; then
 	rm /system/etc/permissions/android.hardware.nfc.xml
 	rm /system/etc/permissions/android.hardware.nfc.hce.xml
 	rm /system/lib/hw/nfc_nci.pn54x.default.so
